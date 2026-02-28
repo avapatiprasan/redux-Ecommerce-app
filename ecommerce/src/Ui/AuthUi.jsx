996d81e67e674726e5eff0b3355be017267fd26c
zuber <avapatidileepkumar@gmail.com>
2026-02-28 10:25:54 +0530

    added

--- ecommerce/src/Ui/AuthUi.jsx
+++ ecommerce/src/Ui/AuthUi.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginApi } from "../Slices/AuthSlice";
 import { useNavigate } from "react-router-dom";
-import "./AuthUi.css";
+import "./AuthUI.css";
 
 const AuthUi = () => {
   const [username, setUsername] = useState("");
